9956ebf5f7a5185b7c34db8bff2e56fe505015a1
Carlos Adriano <carlos.adriano>
2018-09-19 16:17:06 -0300

    Adição do filtro de mediana para ruído sal e pimento

--- segmentacao.py
+++ segmentacao.py
@@ -4,9 +4,52 @@ import time
 import sys
 import os
 
+'''Implementação do filtro de mediana.'''
+def get_median(list):
+    sortedlist = sorted(list)
+    tamanhoLista = len(sortedlist)
+    meio = int(tamanhoLista / 2)
+    if tamanhoLista % 2 == 0:
+        medianA = sortedlist[meio]
+        medianB = sortedlist[meio-1]
+        median = np.double(medianA + medianB) / 2
+    else:
+        median = sortedlist[meio + 1]
+    return median
+
+def getFiltro(tamanhoJanela):
+    filtro = np.zeros((tamanhoJanela, tamanhoJanela), int)
+    return filtro
+
+def median_filter(img, tamFiltro):
+    imgDest = np.zeros_like(img)
+    filtro = getFiltro(tamFiltro)
+    width, height = img.shape[1], img.shape[0]
+    filter_width, filter_height = filtro.shape[0], filtro.shape[1]
+    intensidades = []
+
+    for y in range(height):
+        for x in range(width):
+
+            for filterY in range(int(-(filter_height / 2)), filter_height - 1):
+                for filterX in range(int(-(filter_width / 2)), filter_width - 1):
+
+                    pixel_y = y - filterY
+                    pixel_x = x - filterX
+                    pixel = img[filterY, filterX]
+
+                    if (pixel_y >= 0) and (pixel_y < height) and (pixel_x >= 0) and (pixel_x < width):
+                        pixel = img[pixel_y, pixel_x]
+
+                    intensidades.append(pixel)
+
+            mediana = get_median(intensidades)
+            imgDest[y, x] = mediana
+            intensidades = []
+    return imgDest
 
 def edge_detector(img):
-    img= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #img= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     height, width = img.shape[0], img.shape[1]
     resultado = np.zeros_like(img, dtype=np.float)
     soma = np.zeros_like(img)
@@ -62,10 +105,10 @@ def edge_detector(img):
                 resultado[i, j] = 1
             else:
                 resultado[i, j] = 0
-    cv2.imshow("Detector de Bordas Sobel", resultado)
+    #cv2.imshow("Detector de Bordas Sobel", resultado)
+    return resultado
 
-
-def process_image(imgName):
+def process_edge_detection(imgName, filter_with_median):
     start_time = time.time()
     desImgName = imgName
     imgName = sys.path[0] + '\\imgs\\' + imgName
@@ -74,17 +117,18 @@ def process_image(imgName):
     if not os.path.exists(processedFolder):
         os.makedirs(processedFolder)
 
-    img= cv2.imread(imgName)
+    img = cv2.imread(imgName, cv2.IMREAD_GRAYSCALE)
     print("Processing image '{0}'...".format(imgName))
 
-    destImg = processedFolder + "\\" + desImgName
-    #cv2.imwrite(destImg, img)
-    edge_detector(img)
-    cv2.waitKey(0)
+    if (filter_with_median):
+        img = median_filter(img, 7)
+
+    destImgName = processedFolder + "\\" + desImgName
+    imgDest = edge_detector(img)
+    cv2.imwrite(destImgName, imgDest.astype('uint8') * 255)
     elapsed_time = time.time() - start_time
-    print("Done.")
     print("Done! Elapsed Time: {0}".format(time.strftime("%H:%M:%S", time.gmtime(elapsed_time))))
 
-
 ''' Programa Principal '''
-process_image("image_(1).jpg")
+process_edge_detection("image_(1).jpg", False)
+process_edge_detection("Image_(1a).jpg", True)
